72335875cc6c2b36e7323d996351a261f9a3b2a1
Julien Robert <julien@rbrt.fr>
2022-12-18 20:28:43 +0100

    configure node in `make init`

--- README.md
+++ README.md
@@ -7,10 +7,10 @@
 | `simapp-next` | `next` (a.k.a `main`) | [simapp@main](https://github.com/cosmos/cosmos-sdk/tree/main/simapp)     |
 | `simapp-v047` | `v0.47`               | [simapp@v0.47](https://github.com/cosmos/cosmos-sdk/tree/v0.47.0/simapp) |
 
-| Name  (binary) | Endpoint                                                 | Faucet | RPC (port) | GRPC (port) | REST (port) |
-| -------------- | -------------------------------------------------------- | ------ | ---------- | ----------- | ----------- |
-| `simapp-next`  | [https://next.simapp.zone:443](https://next.simapp.zone) | 80     |            |             |             |
-| `simapp-v047`  | [https://v047.simapp.zone:443](https://v047.simapp.zone) | 80     |            |             |             |
+| Name  (binary) | Endpoint                                             | Faucet | RPC (port) | GRPC (port) | REST (port) |
+| -------------- | ---------------------------------------------------- | ------ | ---------- | ----------- | ----------- |
+| `simapp-next`  | [https://next.simapp.zone](https://next.simapp.zone) | 80     | 1317       | 9090        | 9091        |
+| `simapp-v047`  | [https://v047.simapp.zone](https://v047.simapp.zone) | 80     | 1317       | 9090        | 9091        |
 
 ## Use locally
 
--- scripts/init.sh
+++ scripts/init.sh
@@ -10,6 +10,9 @@ SIMD_BIN=./app/$1/simapp-$1
 SIMD_HOME=./app/$1/.simapp
 CHAIN_ID=$1-1
 
+GENESIS=$SIMD_HOME/config/genesis.json
+APP_CONFIG=$SIMD_HOME/config/app.toml
+
 # build simapp
 cd app/$1
 go build -ldflags "-X github.com/cosmos/cosmos-sdk/version.Name=simd -X github.com/cosmos/cosmos-sdk/version.AppName=simd -X github.com/cosmos/cosmos-sdk/version.Version=$1 -X github.com/cosmos/cosmos-sdk/version.Commit=simapp-zone" -o simapp-$1 ./simd
@@ -23,8 +26,24 @@ $SIMD_BIN keys add root --home $SIMD_HOME
 $SIMD_BIN keys add faucet --home $SIMD_HOME
 $SIMD_BIN init $1 --chain-id $CHAIN_ID --default-denom stake --home $SIMD_HOME
 # update genesis
-$SIMD_BIN genesis add-genesis-account root 10000000000000000stake --keyring-backend test --home $SIMD_HOME
-$SIMD_BIN genesis add-genesis-account faucet 100000000000000stake --keyring-backend test --home $SIMD_HOME
+$SIMD_BIN genesis add-genesis-account root 100000000000000000stake --keyring-backend test --home $SIMD_HOME
+$SIMD_BIN genesis add-genesis-account faucet 1000000000000000stake --keyring-backend test --home $SIMD_HOME
 # create default validator
-$SIMD_BIN genesis gentx root 1000000000000000stake --chain-id $CHAIN_ID --home $SIMD_HOME
+$SIMD_BIN genesis gentx root 10000000000000000stake --chain-id $CHAIN_ID --home $SIMD_HOME
 $SIMD_BIN genesis collect-gentxs --home $SIMD_HOME
+
+# configure genesis
+jq '.app_state.gov.voting_params.voting_period = "3600s"' $GENESIS > $GENESIS # Udpate gov module
+
+# configure node (TODO use confix after https://github.com/cosmos/cosmos-sdk/pull/14342)
+if ! command -v dasel &> /dev/null
+then
+    go install github.com/tomwright/dasel/cmd/dasel@master
+fi
+
+dasel put bool -f $APP_CONFIG "telemetry.enabled" true
+dasel put int -f $APP_CONFIG "telemetry.prometheus-retention-time" 60
+dasel put bool -f $APP_CONFIG "api.enable" true
+dasel put bool -f $APP_CONFIG "api.swagger" true
+dasel put bool -f $APP_CONFIG "api.enabled-unsafe-cors" true
+dasel put bool -f $APP_CONFIG "grpc-web.enabled-unsafe-cors" true
